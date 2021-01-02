baeed2b5c43e3cb172df8c0a1829554c0d085208
Klupea <insatcoma@outlook.fr>
2021-01-02 17:12:50 +0100

    init maven proj

---
+++ src/main/java/com/sifokl/sms/App.java
@@ -0,0 +1,13 @@
+package com.sifokl.sms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

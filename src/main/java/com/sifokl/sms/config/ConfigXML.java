7be6e27e6cfa3bac65a7c0a8ef055fe98e45d1f9
Klupea <insatcoma@outlook.fr>
2021-01-06 22:49:41 +0100

    unmarshall xml

--- src/main/java/com/sifokl/sms/config/ConfigXML.java
+++ src/main/java/com/sifokl/sms/config/ConfigXML.java
@@ -1,15 +1,45 @@
 package com.sifokl.sms.config;
 
+import com.sifokl.sms.values.Values;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
 public class ConfigXML extends Config{
 
 
     @Override
     public void load() {
-
+        load(Values.DEFAULT_CONFIG_SMS_FILE_NAME);
     }
 
     @Override
     public void load(String fileName) {
 
     }
+
+
+    private ConfigXML xmlToConfig(String fileName){
+
+        ConfigXML conf = new ConfigXML();
+
+        try {
+
+            File file = new File(fileName);
+            JAXBContext jaxbContext = JAXBContext.newInstance(ConfigXML.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            conf = (ConfigXML) jaxbUnmarshaller.unmarshal(file);
+
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+        return conf;
+
+    }
+
 }

da866c61652dcddb202bce7ba432f2badbb3ce4a
Klupea <insatcoma@outlook.fr>
2021-01-12 16:56:39 +0100

    catch auth exception when creds are compromised

--- src/main/java/com/sifokl/sms/launcher/Example.java
+++ src/main/java/com/sifokl/sms/launcher/Example.java
@@ -5,6 +5,7 @@ import com.sifokl.sms.config.ConfigTypeFactory;
 import com.sifokl.sms.logging.CustomLogger;
 import com.sifokl.sms.util.StringUtils;
 import com.twilio.Twilio;
+import com.twilio.exception.ApiException;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.type.PhoneNumber;
@@ -25,19 +26,19 @@ public class Example{
 
 
         if(!optConf.isPresent()){
-            logger.severe("Invalid Config : ");
+            logger.severe("Invalid Config : can't read config");
 
             System.exit(-1);
         }
 
         Config conf = optConf.get();
         logger.info(conf.toString());
-
+/*
         logger.info("Credentials (truncated BASE64): ");
         logger.info("ACCOUNT_SID : "+ StringUtils.truncate(StringUtils.toBase64(conf.getACCOUNT_SID()),7));
         logger.info("AUTH_TOKEN : "+StringUtils.truncate(StringUtils.toBase64(conf.getAUTH_TOKEN()),7));
         logger.info("TRIAl_NUMBER : "+conf.getTRIAL_NUMBER());
-
+*/
 
         Twilio.init(conf.getACCOUNT_SID(), conf.getAUTH_TOKEN());
 
@@ -48,9 +49,12 @@ public class Example{
         MessageCreator msgCreator = Message.creator(to , from , body);
 
 
-
-        Message message = msgCreator.create();
-
+        try {
+            Message message = msgCreator.create();
+        }catch(ApiException e){
+            logger.severe("API EXCEPTION : verify auth credentials (could be compromised) : Exception Message : "+e.getMessage());
+            e.printStackTrace();
+        }
 
 
         logger.info("### END of Program ###");
